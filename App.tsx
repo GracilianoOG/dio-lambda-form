18846ad2dca5c79bfe5c9c596db5316b17956e15
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-21 16:44:14 -0300

    Create button to navigate to form

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image source={require("./assets/lambda.png")} />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Open form</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#fa7e14",
+    borderRadius: 8,
+    marginTop: 90,
+    padding: 12,
+    width: 200,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    textTransform: "uppercase",
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: 4,
   },
 });
