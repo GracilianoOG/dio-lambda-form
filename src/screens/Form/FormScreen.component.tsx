095a920ed36116dad25d3a0d911050cefb2e8655
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-21 18:27:57 -0300

    Create Form screen

---
+++ src/screens/Form/FormScreen.component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./FormScreen.style";
+
+const FormScreen = () => {
+  return (
+    <View>
+      <Text>hello world</Text>
+    </View>
+  );
+};
+
+export default FormScreen;
