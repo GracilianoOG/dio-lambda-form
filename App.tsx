4467cd06955a7a1eb88c3ad126618fa98cc2e049
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-21 18:31:53 -0300

    Add navigation wrappers

--- App.tsx
+++ App.tsx
@@ -1,5 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/Home/HomeScreen.component";
+import FormScreen from "./src/screens/Form/FormScreen.component";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name="Form" component={FormScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
