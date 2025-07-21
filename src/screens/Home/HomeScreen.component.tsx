a7d31fd2c158bc3bac8bfdf268c5c34ad30a07f2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-21 18:35:30 -0300

    Navigate to another screen

--- src/screens/Home/HomeScreen.component.tsx
+++ src/screens/Home/HomeScreen.component.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./HomeScreen.style";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-const HomeScreen = () => {
+type RootStackParamList = {
+  Home: undefined;
+  Form: undefined;
+};
+
+type HomeScreenProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList, "Home">;
+};
+
+const HomeScreen = ({ navigation }: HomeScreenProps) => {
   return (
     <View style={styles.container}>
       <Image source={require("../../../assets/lambda.png")} />
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Form")}
+      >
         <Text style={styles.buttonText}>Open form</Text>
       </TouchableOpacity>
     </View>
