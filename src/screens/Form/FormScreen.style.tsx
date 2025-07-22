44c839d993a80dc0c845db5e6635c0dcd151eb51
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-22 17:41:54 -0300

    Center content and style field

--- src/screens/Form/FormScreen.style.tsx
+++ src/screens/Form/FormScreen.style.tsx
@@ -1,5 +1,20 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+  field: {
+    width: "80%",
+  },
+  label: {
+    fontSize: 18,
+    textAlign: "left",
+  },
+  input: {
+    elevation: 1,
+    padding: 20,
+  },
 });
