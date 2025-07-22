fa656300f4c842eaa06d8ca6eb5906893c0c3eda
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-22 18:51:11 -0300

    Destruct Stack

--- App.tsx
+++ App.tsx
@@ -3,15 +3,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/Home/HomeScreen.component";
 import FormScreen from "./src/screens/Form/FormScreen.component";
 
-const Stack = createNativeStackNavigator();
+const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
-        <Stack.Screen name="Form" component={FormScreen}></Stack.Screen>
-      </Stack.Navigator>
+      <Navigator>
+        <Screen name="Home" component={HomeScreen}></Screen>
+        <Screen name="Form" component={FormScreen}></Screen>
+      </Navigator>
     </NavigationContainer>
   );
 }
