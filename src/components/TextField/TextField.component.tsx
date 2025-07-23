574167de1e3844c712a0719782efbe51aa210e66
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-23 13:14:55 -0300

    Import TextFieldProps

--- src/components/TextField/TextField.component.tsx
+++ src/components/TextField/TextField.component.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { KeyboardTypeOptions, Text, TextInput, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./TextField.style";
-import { FormDataType } from "../../utils/types";
-
-type TextFieldProps = {
-  label: string;
-  name: string;
-  formData: FormDataType;
-  setFormData: React.Dispatch<React.SetStateAction<FormDataType>>;
-  keyboardType: KeyboardTypeOptions;
-  placeholder: string;
-};
+import { TextFieldProps } from "./types";
 
 const TextField = ({
   label,
