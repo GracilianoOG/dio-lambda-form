a371eecc03308b1eb3102a61ba82722c7d0d1293
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-23 11:35:04 -0300

    Remove unneeded imports and constants

--- App.tsx
+++ App.tsx
@@ -1,23 +1,4 @@
-import { NavigationContainer } from "@react-navigation/native";
-import {
-  createNativeStackNavigator,
-  NativeStackNavigationOptions,
-} from "@react-navigation/native-stack";
-import HomeScreen from "./src/screens/Home/HomeScreen.component";
-import FormScreen from "./src/screens/Form/FormScreen.component";
 import Routes from "./src/routes";
-
-const { Navigator, Screen } = createNativeStackNavigator();
-
-const sharedOptions: NativeStackNavigationOptions = {
-  headerStyle: { backgroundColor: "#fa7e14" },
-  headerTitleStyle: { color: "#fff" },
-  headerTitleAlign: "center",
-};
-
-const formScreenOptions: NativeStackNavigationOptions = {
-  headerTitle: "Message Us",
-};
 
 export default function App() {
   return <Routes />;
