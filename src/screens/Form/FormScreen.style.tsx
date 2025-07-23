cc4f9c4be7d3553e6572e842e6341a99e1645cfb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-23 14:11:42 -0300

    Removed unused styles

--- src/screens/Form/FormScreen.style.tsx
+++ src/screens/Form/FormScreen.style.tsx
@@ -8,22 +8,6 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 32,
   },
-  field: {
-    width: "100%",
-  },
-  label: {
-    fontSize: 14,
-    fontWeight: "700",
-    letterSpacing: 2.5,
-    marginBottom: 8,
-    textAlign: "left",
-    textTransform: "uppercase",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#9c9c9cff",
-    padding: 16,
-  },
   button: {
     backgroundColor: "#fa7e14",
     padding: 14,
