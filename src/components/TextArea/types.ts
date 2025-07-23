012b9dea0f55dbacb15d6a3849ab27f58f0b9551
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-23 15:39:10 -0300

    Extend TextFieldProps

--- src/components/TextArea/types.ts
+++ src/components/TextArea/types.ts
@@ -1,13 +1,6 @@
-import { KeyboardTypeOptions } from "react-native";
-import { FormDataType } from "../../utils/types";
+import { TextFieldProps } from "../TextField/types";
 
 export type TextAreaProps = {
-  label: string;
-  name: string;
-  formData: FormDataType;
-  setFormData: React.Dispatch<React.SetStateAction<FormDataType>>;
-  keyboardType?: KeyboardTypeOptions;
-  placeholder: string;
   numberOfLines: number;
   maxLength: number;
-};
+} & TextFieldProps;
