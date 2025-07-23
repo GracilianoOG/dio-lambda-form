b5a087f0d20ed64b05172b672323776c51205767
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-23 11:33:38 -0300

    Create stack.routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,38 @@
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationOptions,
+} from "@react-navigation/native-stack";
+import React from "react";
+import HomeScreen from "../screens/Home/HomeScreen.component";
+import FormScreen from "../screens/Form/FormScreen.component";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+const sharedOptions: NativeStackNavigationOptions = {
+  headerStyle: { backgroundColor: "#fa7e14" },
+  headerTitleStyle: { color: "#fff" },
+  headerTitleAlign: "center",
+};
+
+const formScreenOptions: NativeStackNavigationOptions = {
+  headerTitle: "Message Us",
+};
+
+const StackRoutes = () => {
+  return (
+    <Navigator initialRouteName="Home">
+      <Screen
+        name="Home"
+        component={HomeScreen}
+        options={sharedOptions}
+      ></Screen>
+      <Screen
+        name="Form"
+        component={FormScreen}
+        options={{ ...sharedOptions, ...formScreenOptions }}
+      ></Screen>
+    </Navigator>
+  );
+};
+
+export default StackRoutes;
