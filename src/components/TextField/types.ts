25b503633a8b294d1f710a8fe0693071efe5c7fd
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-23 15:30:48 -0300

    Add types for submit related configs

--- src/components/TextField/types.ts
+++ src/components/TextField/types.ts
@@ -1,5 +1,15 @@
-import { KeyboardTypeOptions } from "react-native";
+import {
+  KeyboardTypeOptions,
+  ReturnKeyTypeOptions,
+  SubmitBehavior,
+} from "react-native";
 import { FormDataType } from "../../utils/types";
+
+type SubmitConfig = {
+  returnKeyType: ReturnKeyTypeOptions;
+  submitBehavior: SubmitBehavior;
+  onSubmitEditing?: any;
+};
 
 export type TextFieldProps = {
   label: string;
@@ -8,4 +18,6 @@ export type TextFieldProps = {
   setFormData: React.Dispatch<React.SetStateAction<FormDataType>>;
   keyboardType: KeyboardTypeOptions;
   placeholder: string;
+  ref: any;
+  submitConfig: SubmitConfig;
 };
