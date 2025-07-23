f48c230feb474496f408a9670caddfff6867f52c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-23 11:34:26 -0300

    Return Routes

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
 } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/Home/HomeScreen.component";
 import FormScreen from "./src/screens/Form/FormScreen.component";
+import Routes from "./src/routes";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -19,20 +20,5 @@ const formScreenOptions: NativeStackNavigationOptions = {
 };
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Navigator>
-        <Screen
-          name="Home"
-          component={HomeScreen}
-          options={sharedOptions}
-        ></Screen>
-        <Screen
-          name="Form"
-          component={FormScreen}
-          options={{ ...sharedOptions, ...formScreenOptions }}
-        ></Screen>
-      </Navigator>
-    </NavigationContainer>
-  );
+  return <Routes />;
 }
