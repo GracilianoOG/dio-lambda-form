b2388682e70b574a2a307467dde432e3ed7d0dc0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-23 13:01:54 -0300

    Create TextField component

---
+++ src/components/TextField/TextField.component.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { KeyboardTypeOptions, Text, TextInput, View } from "react-native";
+
+import { styles } from "./TextField.style";
+
+type TextFieldProps = {
+  label: string;
+  name: string;
+  formData: any;
+  setFormData: any;
+  keyboardType: KeyboardTypeOptions;
+  placeholder: string;
+};
+
+const TextField = ({
+  label,
+  name,
+  setFormData,
+  formData,
+  keyboardType,
+  placeholder,
+}: TextFieldProps) => {
+  return (
+    <View style={styles.field}>
+      <Text style={styles.label}>{label}:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType={keyboardType}
+        placeholder={placeholder}
+        onChangeText={data => setFormData({ ...formData, [name]: data })}
+        value={formData.name}
+      />
+    </View>
+  );
+};
+
+export default TextField;
---
+++ src/components/TextField/TextField.style.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  field: {
+    width: "100%",
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: "700",
+    letterSpacing: 2.5,
+    marginBottom: 8,
+    textAlign: "left",
+    textTransform: "uppercase",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#9c9c9cff",
+    padding: 16,
+  },
+});
